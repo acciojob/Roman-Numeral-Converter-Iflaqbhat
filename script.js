685cd8b40c767e937001286eb0f76d0010ee690f
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-12 13:02:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,9 +20,7 @@ function convertToRoman(num) {
     }
     
     return roman;
-    }
     
-    return roman;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
